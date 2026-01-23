0dd12df72859f7129e5453c17ade72661a215eb9
LempiRajala <lempirajala79@gmail.com>
2026-01-23 22:21:25 +0500

    fix: add strong password validation

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -67,11 +67,23 @@ export function validatePassword(password) {
   if (!password) {
     return t`No password given.`;
   }
-  if (password.length < 6) {
-    return t`Password must be at least 6 characters long.`;
+  if (password.length < 8) {
+    return t`Password must be at least 8 characters long.`;
   }
   if (password.length > 60) {
     return t`Password must be shorter than 60 characters.`;
+  }
+  if (!/[A-Z]/.test(password)) {
+    return t`Password must contain at least one uppercase letter`;
+  }
+  if (!/[a-z]/.test(password)) {
+    return t`Password must contain at least one lowercase letter`;
+  }
+  if (!/\d/.test(password)) {
+    return t`Password must contain at least one number`;
+  }
+  if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) {
+    return t`Password must contain at least one special character`;
   }
   return false;
 }
