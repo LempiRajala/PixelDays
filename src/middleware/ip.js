91d5b3a5727215194847fe420085e0d5370a520c
LempiRajala <lempirajala79@gmail.com>
2026-02-18 01:54:24 +0500

    remove ip logs

--- src/middleware/ip.js
+++ src/middleware/ip.js
@@ -63,8 +63,6 @@ export class IP {
       }
     }
     ipString = sanitizeIPString(ipString);
-    console.log(req.headers);
-    console.log(ipString);
     Object.defineProperty(this, 'ipString', { value: ipString });
     return ipString;
   }
