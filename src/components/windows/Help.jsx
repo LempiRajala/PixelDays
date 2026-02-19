e7e064ec28a798ad0dc628ff371cf34a638ec194
KASTA <76002509+Endropr@users.noreply.github.com>
2026-02-20 04:17:26 +0500

    Revise Help component content and contact info
    Updated information about moderation policies and contact channels.

--- src/components/windows/Help.jsx
+++ src/components/windows/Help.jsx
@@ -66,23 +66,21 @@ const Help = () => {
         alt="ppfun"
         src={cdn`/logo.png`}
         ref={refCallback}
-      />
+       />
       <p>PixelDays is a pixel game for you to have fun with your friends, create art, countries, factions, expand, and wage war against other nations!</p>
       <p>PixelDays stands out among its competitors because it has less harassment, discrimination, and almost complete freedom of speech.</p>
       <br/>
       <p>Here's some information about us:</p>
       <p style={{ paddingLeft: '10px' }}>1) We guarantee freedom of expression in our project. There is almost no harassment from moderation for your stance or ideals. </p>
-      <p style={{ paddingLeft: '10px' }}>2) We only target those who engage in destructive behavior in society (propaganda of debauchery, LGBT, feminism, pedophilia, and other socially unacceptable movements).</p>
-      <p style={{ paddingLeft: '10px' }}>3) We are a website with conservative and appropriate ideas. Supporters of left-wing movements that promote gender diversity and multiculturalism: please leave this website.</p>
-      <p style={{ paddingLeft: '10px' }}>4) We do not allow moderation to abuse its authority. For example, if a moderator is Russian or Ukrainian, they should not be more loyal to their own country than to players from other factions.</p>
-      <p style={{ paddingLeft: '10px' }}>5) We listen to the players' opinions and try to implement their ideas and ambitions. You help us to become better!</p>
+      <p style={{ paddingLeft: '10px' }}>2) We do not allow moderation to abuse its authority. For example, if a moderator is Russian or Ukrainian, they should not be more loyal to their own country than to players from other factions.</p>
+      <p style={{ paddingLeft: '10px' }}>3) We listen to the players' opinions and try to implement their ideas and ambitions. You help us to become better!</p>
 
       <p>Our main canvas is a huge world map, you can draw your art, build your countries and create the history of our site!</p>
       <p>The cooldown on the site is 1/2, the reserve of pixels (recharge) - 120 seconds.</p>
 
       <p>PixelDays is an experiment in the world of pixels to find out what will happen if you give a person the right to self-expression, bypassing censorship and control from the administration. Most websites persecute people for their views and punish them severely, even to the point of wiping out an entire country.</p>
 
-      <p>Learn more about us on our Discord channel: [link]</p>
+      <p> Learn more about us on our Telegram channel: https://t.me/pixeldaysfun </p>
       <h3>{t`Identifiers`}</h3>
       <p>{t`If you talk to moderators or administrators, you might get asked for one of the following identifiers:`}</p>
       <p>IID</p>
